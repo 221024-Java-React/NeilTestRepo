61edf1738ae3f3a5cb1782f54eedc9b1701ee659
Dtech7 <neild.felix@gmail.com>
2022-10-25 13:10:26 -0400

    first lab

---
+++ Lab1.java
@@ -0,0 +1,21 @@
+
+public class Lab1 {
+	public static void main(String[] args) {
+		//defining variables
+		boolean boo = true;
+		byte maxByte = 127;
+		short minShort = -32768;
+		int maxInt =  2147483647;
+		long javaFlex = 505000000000L;
+		float pie = 3.14159f;
+		double doubleYum = 3.14159 * 2;
+		//Printing variables
+		System.out.println("boolean: " + boo);
+		System.out.println("byte: " + maxByte);
+		System.out.println("short: "+ minShort);
+		System.out.println("int: "+ maxInt);
+		System.out.println("long: "+ javaFlex);
+		System.out.println("float: " + pie);
+		System.out.println("double: " + doubleYum);
+	}
+}
